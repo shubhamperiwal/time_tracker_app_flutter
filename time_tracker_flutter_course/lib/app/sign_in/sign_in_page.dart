ab9e03141134ec10a8df988830ad9c6a20e2f09b
Shubham Periwal <shubhamperiwal98@gmail.com>
2020-06-09 21:57:37 +0800

    email sign in page

--- time_tracker_flutter_course/lib/app/sign_in/sign_in_page.dart
+++ time_tracker_flutter_course/lib/app/sign_in/sign_in_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:time_tracker_flutter_course/app/sign_in/sign_in_button.dart';
 import 'package:time_tracker_flutter_course/app/sign_in/social_sign_in_button.dart';
 import 'package:time_tracker_flutter_course/services/auth.dart';
+import 'package:time_tracker_flutter_course/app/sign_in/email_sign_in_page.dart';
 
 class SignInPage extends StatelessWidget {
 
@@ -26,6 +27,16 @@ class SignInPage extends StatelessWidget {
     }
   }
 
+  void _signInWithEmail(BuildContext context){
+    // show email sign in page
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        // fullscreenDialog: true,
+        builder: (context) => EmailSignInPage(),
+      )
+    );
+  }
+
 	@override
 	Widget build(BuildContext context) {
 		return Scaffold(
@@ -33,12 +44,12 @@ class SignInPage extends StatelessWidget {
 			title: Text('Time Tracker'),
 			elevation: 5.0, //adds a shadow
 			),
-			body: _buildContent(),
+			body: _buildContent(context),
 			backgroundColor: Colors.grey[200],
 		);
 	}
 
-  Widget _buildContent() {
+  Widget _buildContent(BuildContext context) {
     return Padding(
         padding: EdgeInsets.all(16.0),
         child: Column(
@@ -53,7 +64,7 @@ class SignInPage extends StatelessWidget {
                 fontWeight: FontWeight.w600,
               ),
 			),
-            SizedBox(height: 48.0),
+      SizedBox(height: 48.0),
 			SocialSignInButton(
         assetName: 'images/google-logo.png',
         text: 'Sign in with Google',
@@ -61,11 +72,12 @@ class SignInPage extends StatelessWidget {
         color: Colors.white,
         onPressed: _signInWithGoogle,
       ),
+      SizedBox(height: 8.0),
 			SignInButton(
 				text: 'Sign in with email',
 				textColor: Colors.white,
 				color: Colors.teal[700],
-				onPressed: () {},
+				onPressed: () => _signInWithEmail(context),
 			),
 			SizedBox(height: 8.0),
 			Text(
